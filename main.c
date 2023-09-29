1308ef40272d245e8b5bf001b2f84eaa9d65556f
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-09-29 11:50:22 +0600

    I have a upload programming c file

---
+++ main.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+// Floating And Double Charecter Number----------)
+
+    
+    #include<stdio.h>
+   int main(){
+    float num1 = 10.5;
+    double num2 = 10.545555556666;
+    char ch = 'a';
+
+     printf("num1 = %.1f\n",num1);
+     printf("num2 = %lf\n",num2);
+     printf("ch = %c",ch);
+   }
+   
+
+  // Ends Of Number -----------)
+
+ 
+  
